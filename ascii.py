d5df9cad0498269c9c44922dc64af597cd97665f
Bekah McCurry <rebekah.a.mccurry@icloud.com>
2021-03-30 16:33:13 -0700

    Include alternate canvas/rectangle classes with canvas inverted.

--- ascii.py
+++ ascii.py
@@ -1,18 +1,190 @@
-# 0123456789
-#0 
-#1 ***   *** 
-#2 * *   * *
-#3 ***   ***
-#4 
-#5 =========
-#6 =       = 
-#7 ========= 
-#8 
+# # 0123456789
+# #0 
+# #1 ***   *** 
+# #2 * *   * *
+# #3 ***   ***
+# #4 
+# #5 =========
+# #6 =       = 
+# #7 ========= 
+# #8 
 
+
+# class Canvas():
+#   def __init__(self, height, width, empty='.'):
+#     # self.data = [[empty]*width for _ in range(height)]
+#     self.height = height
+#     self.width = width
+#     self.empty = empty
+#     self.shapes = []
+
+#   def add(self, shape):
+#     self.shapes.append(shape)
+
+#   def clear(self):
+#     self.shapes = []
+
+#   def render(self):
+#     data = [[self.empty]*self.width for _ in range(self.height)]
+#     for shape in self.shapes:
+#       shape.fill(data)
+#     for row in data:
+#       print(''.join(row))
+
+
+# class Rectangle():
+#   def __init__(self, start_x, start_y, end_x, end_y, fill_char):
+#     self.start_x = start_x
+#     self.start_y = start_y
+#     self.end_x = end_x
+#     self.end_y = end_y
+#     self.fill_char = fill_char
+
+#   def __repr__(self):
+#     return (
+#         f'Rectangle({self.start_x}, {self.start_y}, {self.end_x}, '
+#         f'{self.end_y}, {repr(self.fill_char)})')
+
+#   def fill(self, data):
+#     for y in range(self.start_y, self.end_y+1):
+#       for x in range(self.start_x, self.end_x+1):
+#         data[y][x] = self.fill_char
+  
+#   def change_char(self, char):
+#     self.fill_char = char
+
+#   def translate(self, axis, nums):
+#     if axis == 'y': #up or down
+#       self.start_y += nums
+#       self.end_y += nums
+#     elif axis == 'x': #left or right
+#       self.start_x += nums
+#       self.end_x += nums
+
+
+# c = Canvas(9, 10)
+# rectangles = [
+#   Rectangle(1, 1, 3, 3, '*'),
+#   Rectangle(7, 1, 9, 3, '*'),
+#   Rectangle(1, 5, 9, 7, '='),
+#   Rectangle(2, 2, 2, 2, ' '),
+#   Rectangle(8, 2, 8, 2, ' '),
+#   Rectangle(2, 6, 8, 6, ' '),
+#   ]
+# for r in rectangles:
+#   c.add(r) 
+# c.render()
+
+# print(c.shapes)
+# rectangles[0].translate('y', 1)
+# rectangles[3].translate('y', 1)
+# c.render()
+# rectangles[2].change_char('_')
+# c.render()
+# print(c.shapes)
+
+# *********INVERTED CANVAS - ON RECTANGLE.FILL()*********
+# # 0123456789
+# #8 
+# #7 ***   *** 
+# #6 * *   * *
+# #5 ***   ***
+# #4 
+# #3 =========
+# #2 =       = 
+# #1 ========= 
+# #0 
+
+
+# class Canvas():
+#   def __init__(self, height, width, empty='.'):
+#     # self.data = [[empty]*width for _ in range(height)]
+#     self.height = height
+#     self.width = width
+#     self.empty = empty
+#     self.shapes = []
+
+#   def add(self, shape):
+#     self.shapes.append(shape)
+
+#   def clear(self):
+#     self.shapes = []
+
+#   def render(self):
+#     data = [[self.empty]*self.width for _ in range(self.height)]
+#     for shape in self.shapes:
+#       shape.fill(data)
+#     for row in data:
+#       print(''.join(row))
+
+
+# class Rectangle():
+#   def __init__(self, start_x, start_y, end_x, end_y, fill_char):
+#     self.start_x = start_x
+#     self.start_y = start_y
+#     self.end_x = end_x
+#     self.end_y = end_y
+#     self.fill_char = fill_char
+
+#   def __repr__(self):
+#     return (
+#         f'Rectangle({self.start_x}, {self.start_y}, {self.end_x}, '
+#         f'{self.end_y}, {repr(self.fill_char)})')
+
+#   def fill(self, data):
+#     max_y = len(data) - 1
+#     for y in range(self.start_y, self.end_y+1):
+#       for x in range(self.start_x, self.end_x+1):
+#         data[max_y - y][x] = self.fill_char
+  
+#   def change_char(self, char):
+#     self.fill_char = char
+
+#   def translate(self, axis, nums):
+#     if axis == 'y': #up or down
+#       self.start_y += nums
+#       self.end_y += nums
+#     elif axis == 'x': #left or right
+#       self.start_x += nums
+#       self.end_x += nums
+
+
+# c = Canvas(9, 10)
+# rectangles = [
+#   Rectangle(1, 5, 3, 7, '*'),
+#   Rectangle(7, 5, 9, 7, '*'),
+#   Rectangle(1, 1, 9, 3, '='),
+#   Rectangle(2, 6, 2, 6, ' '),
+#   Rectangle(8, 6, 8, 6, ' '),
+#   Rectangle(2, 2, 8, 2, ' '),
+#   ]
+# for r in rectangles:
+#   c.add(r) 
+# c.render()
+
+# print(c.shapes)
+# rectangles[0].translate('y', 1)
+# rectangles[3].translate('y', 1)
+# c.render()
+# rectangles[2].change_char('_')
+# c.render()
+# print(c.shapes)
+
+# *********INVERTED CANVAS - ON CANVAS.RENDER()**********
+# 0123456789
+#8 
+#7 ***   *** 
+#6 * *   * *
+#5 ***   ***
+#4 
+#3 =========
+#2 =       = 
+#1 ========= 
+#0 
 
 class Canvas():
   def __init__(self, height, width, empty='.'):
-    # self.data = [[empty]*width for _ in range(height)]
+    # self.data = [[1]*width for i in range(height, -1, -1)]
     self.height = height
     self.width = width
     self.empty = empty
@@ -25,11 +197,15 @@ class Canvas():
     self.shapes = []
 
   def render(self):
-    data = [[self.empty]*self.width for i in range(self.height)]
+    self.data = [[self.empty]*self.width for _ in range(self.height)]
     for shape in self.shapes:
-      shape.fill(data)
-    for row in data:
-      print(''.join(row))
+      shape.fill(self) #something here? why wont the rectangle print at like (1,1) instead of still printing at the old location (7,1).
+    for row in self.data:
+      print(''.join (row))
+
+  def set_pixel(self, x, y, val):
+    if 0 <= x < self.width and 0 <= y < self.height:
+      self.data[self.height - y - 1][x] = val
 
 
 class Rectangle():
@@ -45,11 +221,12 @@ class Rectangle():
         f'Rectangle({self.start_x}, {self.start_y}, {self.end_x}, '
         f'{self.end_y}, {repr(self.fill_char)})')
 
-  def fill(self, data):
-    for x in range(self.start_x, self.end_x+1):
-      for y in range(self.start_y, self.end_y+1):
-        data[y][x] = self.fill_char
-  
+  def fill(self, canvas):
+    for y in range(self.start_y, self.end_y+1): 
+      for x in range(self.start_x, self.end_x+1): 
+        canvas.set_pixel(x, y, self.fill_char)
+        
+
   def change_char(self, char):
     self.fill_char = char
 
@@ -61,22 +238,22 @@ class Rectangle():
       self.start_x += nums
       self.end_x += nums
 
-
 c = Canvas(9, 10)
 rectangles = [
-  Rectangle(1, 1, 3, 3, '*'),
-  Rectangle(7, 1, 9, 3, '*'),
-  Rectangle(1, 5, 9, 7, '='),
-  Rectangle(2, 2, 2, 2, ' '),
-  Rectangle(8, 2, 8, 2, ' '),
-  Rectangle(2, 6, 8, 6, ' '),
+  Rectangle(1, 5, 3, 7, '*'),
+  Rectangle(7, 5, 9, 7, '*'),
+  Rectangle(1, 1, 9, 3, '='),
+  Rectangle(2, 6, 2, 6, ' '),
+  Rectangle(8, 6, 8, 6, ' '),
+  Rectangle(2, 2, 8, 2, ' '),
   ]
 for r in rectangles:
   c.add(r) 
-
 c.render()
+
 print(c.shapes)
-rectangles[0].translate('y', 2)
+rectangles[0].translate('y', 1)
+rectangles[3].translate('y', 1)
 c.render()
 rectangles[2].change_char('_')
 c.render()
@@ -130,14 +307,3 @@ print(c.shapes)
 # Make sure you do not render any characters that are out of bounds.
 
 # Feel free to make decisions/assumptions about things you have questions about and please document these decisions (using comments in your solution is fine).
-
-  # def translate(self, axis, nums):
-  #   if axis == 'y': #up or down
-  #       self.update_location(nums)
-  #         if positive, moves up, if negative, moves down. x start/end stays the same
-
-  #   elif axis == 'x': #left or right
-  #       self.start_x = self.start_x + int(nums)
-  #         if positive, moves right, if negative, moves left. y start/end stays the same
-
-  #   c.render()
